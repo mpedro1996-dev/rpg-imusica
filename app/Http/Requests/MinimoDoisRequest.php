6032fe3de19cd6a5de426483c143455594174332
Pedro Moura <pedrommoura1996@gmail.com>
2019-03-20 21:23:55 -0300

    Adicionando novas regras

--- app/Http/Requests/MinimoDoisRequest.php
+++ app/Http/Requests/MinimoDoisRequest.php
@@ -3,6 +3,7 @@
 namespace RPGImusica\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 use Symfony\Component\VarDumper\VarDumper;
 
 class MinimoDoisRequest extends FormRequest
@@ -24,15 +25,22 @@ class MinimoDoisRequest extends FormRequest
      */
     public function rules()
     {
+
         $validation = [
             "ids"=>[],
             "personagens"=>[]
         ];
         if($this->get('ids')!=null){
             $validation['ids'] = ['required','array','min:2'];
+            $validation['ids.0']=['required','integer'];
+            $validation['ids.1']=['required','integer'];
         }
         if($this->get('personagens')!=null){
             $validation['personagens'] = ['required','array','min:2'];
+            $validation['personagens.0.id'] = ['required'];
+            $validation['personagens.0.iniciativa'] = ['required','integer',Rule::in([0,1])];
+            $validation['personagens.1.id'] = ['required'];
+            $validation['personagens.1.iniciativa'] = ['required','integer',Rule::in([0,1])];
         }
 
         return $validation;
