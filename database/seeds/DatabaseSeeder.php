c02ba66dd2076df9fd9bd08b431d082f773dc5c2
Pedro Moura <pedrommoura1996@gmail.com>
2019-03-20 21:54:04 -0300

    Readme.md

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 //         $this->call(UsersTableSeeder::class);
-//         $this->call(DadoSeeder::class);
-//         $this->call(ArmaSeeder::class);
-//         $this->call(RacaSeeder::class);
+         $this->call(DadoSeeder::class);
+         $this->call(ArmaSeeder::class);
+         $this->call(RacaSeeder::class);
     }
 }
